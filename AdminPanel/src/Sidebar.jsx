401cf5e74bb896bcb643b92a9899dc518b7e5aa1
Amritpal <amritpal15k@gmail.com>
2024-04-27 12:40:38 +0530

    Added Sidebar

--- AdminPanel/src/Sidebar.jsx
+++ AdminPanel/src/Sidebar.jsx
@@ -1,21 +1,60 @@
 import React from 'react'
-import { BsCart3 } from 'react-icons/bs'
+import {BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill,BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
 
 function Sidebar() {
   return (
     <aside id='sidebar'>
       <div className='sidebar-title'>
         <div className='sidebar-brand'>
-          <BsCart3  className='icon_header'/> ADMIN
+          <BsCart3  className='icon_header'/>ADMIN
         </div>
         <span className='icon close-icon'>X</span>
       </div>
-      <ul className='sidebarlist'>
+      <ul className='sidebar-list'>
+
         <li className='sidebar-list-item'>
           <a href="">
-            <BsCart3 className='icon'/> Dashboard
+            <BsGrid1X2Fill className='icon'/> Dashboard
           </a>
         </li>
+
+        <li className='sidebar-list-item'>
+          <a href="">
+            <BsFillArchiveFill className='icon'/> Products
+          </a>
+        </li>
+
+        <li className='sidebar-list-item'>
+          <a href="">
+            <BsFillGrid3X3GapFill className='icon'/> Categories
+          </a>
+        </li>
+
+        <li className='sidebar-list-item'>
+          <a href="">
+            <BsPeopleFill className='icon'/> Customer
+          </a>
+        </li>
+
+        <li className='sidebar-list-item'>
+          <a href="">
+            <BsListCheck className='icon'/> Inventory
+          </a>
+        </li>
+
+        <li className='sidebar-list-item'>
+          <a href="">
+            <BsMenuButtonWideFill className='icon'/> Reports
+          </a>
+        </li>
+
+        <li className='sidebar-list-item'>
+          <a href="">
+            <BsFillGearFill className='icon'/> Setings
+          </a>
+        </li>
+
       </ul>
     </aside>
   )
