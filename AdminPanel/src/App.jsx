455b35a5991a2da543b58ce4feb723656b3e2aea
Amritpal <amritpal15k@gmail.com>
2024-05-08 13:11:10 +0530

    done

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
-import { Provider } from 'react-redux'
-
-import './App.css'
-import Header from './Header' 
-import Sidebar from './Sidebar'
-import Home from './Home'
+import { useState } from 'react';
+import './App.css';
+import Header from './Header'; 
+import Sidebar from './Sidebar';
+import Home from './Home';
 
 function App() {
+  const [openSideBarToggle, setOpenSideBarToggle] = useState(false);
 
-  const [openSideBarToggle , setOpenSideBarToggle]= useState(false);
+  const openSidebar = () => {
+    setOpenSideBarToggle(!openSideBarToggle);
+  };
 
-  const openSidebar= ()=>{
-    setOpenSideBarToggle(!openSideBarToggle)
-  }
   return (
-    <div className="grid-container">
-      <Header openSidebar={openSidebar}/>
-      <Sidebar openSideBarToggle={openSideBarToggle} openSidebar={openSidebar}/>
+  
+      <div className="grid-container">
+        <Header openSidebar={openSidebar} />
+        <Sidebar openSideBarToggle={openSideBarToggle} openSidebar={openSidebar} />
       <Home />
-
-    </div>
-  )
+      </div>
+   
+  );
 }
 
-export default App
+export default App;
